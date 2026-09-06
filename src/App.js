87ddf0acacc6ed75fc2e22c5258505e05863d11b
preethi-ramadurgam <preethi.ramadurgam@gmail.com>
2026-09-06 07:27:25 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-3xl font-bold text-green-800'>Hey Everyone !Let's Build Netflix GPT</div>
+  );
+}
+
+export default App;
